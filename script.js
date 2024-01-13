dbf483a184b66e33b6bdc1ca1ebde3e7d1a58358
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2024-01-13 04:31:21 +0000

    examples

---
+++ script.js
@@ -0,0 +1,11 @@
+Const accountId =”87685”
+Let accountEmail = “@abcgmail.com”
+Var accountpassword = “74986907”
+accountCity = “Himachal”
+ let accountState;
+
+accountEmail = “@abcDgmail.com”
+accountpassword = “7489678”
+accountcity = “Jaipur”
+ 
+console.table([accountId, accountEmail, accountpassword, accountState, accountCity])
