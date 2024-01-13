6d5d58ea6462efc5e2df363c004b18d79f6b663f
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2024-01-13 04:49:52 +0000

    file added

---
+++ basic/varibles.js
@@ -0,0 +1,13 @@
+ 
+const accountId = "87685"
+let accountEmail = "@abcgmail.com"
+var accountpassword = "74986907"
+accountCity = "Himachal"
+ let accountState;
+console.log(accountId)
+
+accountEmail = "@abcDgmail.com"
+accountPassword = "7489678"
+accountCity = "Jaipur"
+ 
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
