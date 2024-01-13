88b351c05bf797283b080b7c7786fbe05c3f32dd
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2024-01-13 13:20:06 +0000

    add file

---
+++ basic/conversion.js
@@ -0,0 +1,12 @@
+//let score = "33abc"
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let x = 1
+let booleanIsLoggedIn = Boolean(x)
+console.log(booleanIsLoggedIn);
