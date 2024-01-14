56847fdd9b0a23f91f576c51935ed95cf78fee68
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2024-01-14 12:24:30 +0000

    add files

--- basic/conversion.js
+++ basic/conversion.js
@@ -7,6 +7,26 @@
 //console.log(valueInNumber);
 
 
-let x = 1
-let booleanIsLoggedIn = Boolean(x)
-console.log(booleanIsLoggedIn);
+//let x = 1
+//let booleanIsLoggedIn = Boolean(x)
+//console.log(booleanIsLoggedIn);
+
+
+//let x = "hello"
+//let y = " world"
+//let z = x + y
+//console.log(z);
+
+//console.log(1 + 2); //3
+//console.log(1 + "2");  //12
+//console.log("1" + 2);  //12
+//console.log("1" + "2");   //12
+
+
+//console.log("1" + 2 + 2);
+
+//console.log(1 + 2 + "2")
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
