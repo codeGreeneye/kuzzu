e1cbd05bd20917e0aff05cb2b28173bd2823a3cb
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2024-01-15 08:31:26 +0000

    num and maths

---
+++ basic/numbers.js
@@ -0,0 +1,20 @@
+const x = 400
+//console.log(x)
+const balnce = new Number(100)
+//console.log(balnce);
+
+//console.log(balnce.toString().length);
+//console.log(balnce.toFixed(1));
+
+
+
+const otherNumber = 100000
+//console.log(otherNumber.toLocaleString('en-IN'))
+
+
+//console.log(Math);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
--- basic/string.js
+++ basic/string.js
@@ -4,15 +4,13 @@
  
 
 const gameName = new String(`hello world`)
-console.log(gameName[0]);     
-console.log(gameName.__proto__);
-console.log(gameName.length); 
-console.log(gameName.toUpperCase()); 
-console.log(gameName.charAt(3)); 
-console.log(gameName.indexOf('e')); 
+//console.log(gameName[0]);     
+//console.log(gameName.__proto__);
+//console.log(gameName.length); 
+//console.log(gameName.toUpperCase()); 
+//console.log(gameName.charAt(3)); 
+//console.log(gameName.indexOf('e')); 
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const oldString = gameName.slice(-8, 4)
-console.log(oldString);
